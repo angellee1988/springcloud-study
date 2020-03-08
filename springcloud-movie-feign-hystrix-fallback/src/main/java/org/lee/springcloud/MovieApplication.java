8b3333d37b561a420a70e2c2505edb9c02b7b7be
xingjiang.li <xingjiang.li@ttpai.cn>
2020-03-08 23:17:52 +0800

    commit

--- springcloud-movie-feign-hystrix-fallback/src/main/java/org/lee/springcloud/MovieApplication.java
+++ springcloud-movie-feign-hystrix-fallback/src/main/java/org/lee/springcloud/MovieApplication.java
@@ -1,17 +1,28 @@
 package org.lee.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients
-@EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MovieApplication {
+
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registration.addUrlMappings("/hystrix.stream");
+        return registration;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MovieApplication.class, args);
--- springcloud-movie-hystrix-turbine/src/main/java/org/lee/springcloud/MovieApplication.java
+++ springcloud-movie-hystrix-turbine/src/main/java/org/lee/springcloud/MovieApplication.java
@@ -6,10 +6,12 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableHystrix
+@EnableTurbine
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MovieApplication {
