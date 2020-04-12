60801945d98fdf589c776e4eff9d9ed846fd1e3c
xingjiang.li <xingjiang.li@ttpai.cn>
2020-04-12 16:55:51 +0800

    update

---
+++ springcloud-config-client-refresh-bus/src/main/java/org/lee/springcloud/RefreshBusApplication.java
@@ -0,0 +1,12 @@
+package org.lee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefreshBusApplication.class, args);
+    }
+}
