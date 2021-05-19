c8cc6c2f87b7624a4de96189d100473068ecbaec
river <angellee1988@163.com>
2021-05-19 22:45:10 +0800

    springcloud gateway

---
+++ springcloud-gateway-nacos-client/src/main/java/org/lee/springcloud/GatewayClientApplication.java
@@ -0,0 +1,15 @@
+package org.lee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author river106 on 2021/5/19
+ **/
+@SpringBootApplication
+public class GatewayClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayClientApplication.class, args);
+    }
+}
