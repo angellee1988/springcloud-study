e4d1223fd1f4fe027f4871a65e27d47da86c024d
river <angellee1988@163.com>
2021-05-20 23:19:57 +0800

    gateway dynamic router

---
+++ springcloud-gateway-dynamic-route/nacos-consumer/src/main/java/springcloud/NacosConsumerApplication.java
@@ -0,0 +1,15 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author river106 on 2021/5/20
+ **/
+@SpringBootApplication
+public class NacosConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConsumerApplication.class, args);
+    }
+}
---
+++ springcloud-gateway-dynamic-route/nacos-provider/src/main/java/org/lee/springcloud/NacosProviderApplication.java
@@ -0,0 +1,15 @@
+package org.lee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author river106 on 2021/5/20
+ **/
+@SpringBootApplication
+public class NacosProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+}
